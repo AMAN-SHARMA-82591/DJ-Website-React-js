1fd855929a98787a47562e634ca48a7b027d9fb0
Aman <aman82591@gmail.com>
2025-04-09 19:17:48 +0530

    Fixed responsiveness of different components.

--- src/navbar.js
+++ src/navbar.js
@@ -4,32 +4,32 @@ import "./App.css";
 
 function navbar() {
   return (
-    <div class="navbar-section">
-      <nav class="nav-link-main">
+    <div className="navbar-section">
+      <nav className="nav-link-main">
         <div className="main-logo">
           <NavLink className="logo-nav" to="/">
             DJ<span>.</span>
           </NavLink>
         </div>
 
-        <ul class="ul-section">
-          <li class="main-multiple-sec-container">
+        <ul className="ul-section">
+          <li className="main-multiple-sec-container">
             <NavLink className="nav-link" to="/home">
               Home
             </NavLink>
           </li>
-          <li class="main-multiple-sec-container">
+          <li className="main-multiple-sec-container">
             <NavLink className="nav-link active" to="/events">
               Events
               {/* <div className="hidden-items shows"></div> */}
             </NavLink>
           </li>
-          {/* <li class="main-multiple-sec-container">
+          {/* <li className="main-multiple-sec-container">
             <NavLink className="nav-link" to="/about">
               About
             </NavLink>
           </li> */}
-          <li class="main-multiple-sec-container">
+          <li className="main-multiple-sec-container">
             <NavLink className="nav-link" to="/contact">
               Contact
             </NavLink>
